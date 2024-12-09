e3811b16fdff30630f0bdf6076cdb013c0bd13f5
Daniela <danielaportoaguilera@gmail.com>
2024-12-09 19:04:46 -0300

    validacion trayecto

--- app/models/trayecto.rb
+++ app/models/trayecto.rb
@@ -2,4 +2,7 @@ class Trayecto < ApplicationRecord
   belongs_to :tipo_carga
   belongs_to :vehículo
   belongs_to :user
+
+  validates :descripcion, :tipo_carga_id, :destino, :fecha_salida, :fecha_llegada, :vehiculo_id, :user_id, presence: true
+  validates :fecha_llegada, comparison: {greater_than: :fecha_salida, message: “La fecha de llegada no puede ser antes de salida”}
 end
